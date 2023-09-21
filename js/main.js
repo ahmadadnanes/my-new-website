6125301da152f3ad9e2badcc04908b7be91dc853
ahmad adnan <aa264048@gmail.com>
2023-09-21 05:35:10 +0300

    add animation

--- js/main.js
+++ js/main.js
@@ -39,3 +39,22 @@ up.onclick = () =>{
         behavior:"smooth"
     })
 }
+
+
+// reveal animation
+
+window.addEventListener('scroll',reveal);
+
+function reveal() {
+    let reveals = document.querySelectorAll(".reveal");
+    for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+    } else {
+        reveals[i].classList.remove("active");
+    }
+    }
+  }
